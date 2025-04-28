7fbe168fc95c9b668d283970a8ac905d150ffa50
yh-skku <yh03061@g.skku.edu>
2025-04-28 22:27:25 +0900

    implement: study controller, service, repository

---
+++ src/main/java/com/example/comitserver/config/StudyController.java
@@ -0,0 +1,107 @@
+package com.example.comitserver.config;
+
+import com.example.comitserver.dto.CustomUserDetails;
+import com.example.comitserver.dto.ServerResponseDTO;
+import com.example.comitserver.dto.StudyRequestDTO;
+import com.example.comitserver.dto.StudyResponseDTO;
+import com.example.comitserver.entity.StudyEntity;
+import com.example.comitserver.repository.StudyRepository;
+import com.example.comitserver.service.StudyService;
+import com.example.comitserver.utils.ResponseUtil;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api")
+public class StudyController {
+
+    private final StudyService studyService;
+    private final ModelMapper modelMapper;
+    private final StudyRepository studyRepository;
+
+    @Autowired
+    public StudyController(StudyService studyService, ModelMapper modelMapper, StudyRepository studyRepository) {
+        this.studyService = studyService;
+        this.modelMapper = modelMapper;
+        this.studyRepository = studyRepository;
+    }
+
+    @GetMapping("/studies")
+    public ResponseEntity<ServerResponseDTO> getStudies() {
+        List<StudyEntity> allStudies = studyService.showAllStudies();
+        List<StudyResponseDTO> allStudiesDTO = allStudies.stream().map(entity -> modelMapper.map(entity, StudyResponseDTO.class)).collect(Collectors.toList());
+
+        return ResponseUtil.createSuccessResponse(allStudiesDTO, HttpStatus.OK);
+        //return ResponseEntity.ok(allStudiesDTO);
+    }
+
+    @GetMapping("/studies/{id}")
+    public ResponseEntity<ServerResponseDTO> getStudyById(@PathVariable Long id) {
+        if (studyRepository.findById(id).isEmpty()) return ResponseUtil.createErrorResponse(HttpStatus.NOT_FOUND, "Study/CannotFindId", "study with that id not found");
+
+        StudyEntity study = studyService.showStudy(id);
+
+        return ResponseUtil.createSuccessResponse(study, HttpStatus.OK);
+        //return ResponseEntity.ok(modelMapper.map(study, StudyResponseDTO.class));
+    }
+
+    @PostMapping("/studies")
+    public ResponseEntity<ServerResponseDTO> postStudy(@RequestBody StudyRequestDTO studyRequestDTO, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        StudyEntity newStudy = studyService.createStudy(studyRequestDTO, customUserDetails);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newStudy.getId())
+                .toUri();
+
+        return ResponseUtil.createSuccessResponse(modelMapper.map(newStudy, StudyResponseDTO.class), HttpStatus.CREATED, location);
+        //return ResponseEntity.created(location).body(modelMapper.map(newStudy, StudyResponseDTO.class));
+    }
+
+    @PutMapping("/studies/{id}")
+    public ResponseEntity<ServerResponseDTO> putStudy(@PathVariable Long id, @RequestBody StudyRequestDTO studyRequestDTO, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        if (studyRepository.findById(id).isEmpty()) return ResponseUtil.createErrorResponse(HttpStatus.NOT_FOUND, "Study/CannotFindId", "study with that id not found");
+
+        if (studyService.identification(id, customUserDetails)) {
+            StudyEntity updatedStudy = studyService.updateStudy(id, studyRequestDTO);
+
+            if (updatedStudy == null) {
+                return ResponseUtil.createErrorResponse(HttpStatus.NOT_FOUND, "Study/CannotFindId", "the result of updating study is null");
+                //return ResponseEntity.notFound().build();
+            }
+
+            return ResponseUtil.createSuccessResponse(modelMapper.map(updatedStudy, StudyResponseDTO.class), HttpStatus.OK);
+            //return ResponseEntity.ok(modelMapper.map(updatedStudy, StudyResponseDTO.class));
+        }
+        else return ResponseUtil.createErrorResponse(HttpStatus.FORBIDDEN, "Study/PermissionDenied", "the user does not have permission to update this study");
+                //ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+    }
+
+//    @DeleteMapping("/studies/{id}")
+//    public ResponseEntity<ServerResponseDTO> deleteStudy(@PathVariable Long id, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+//        if (studyRepository.findById(id).isEmpty()) return ResponseUtil.createErrorResponse(HttpStatus.NOT_FOUND, "Study/CannotFindId", "study with that id not found");
+//
+//        if(studyService.identification(id, customUserDetails)) {
+//            StudyEntity deletedStudy = studyService.showStudy(id);
+//            StudyResponseDTO studyResponseDTO = modelMapper.map(deletedStudy, StudyResponseDTO.class);
+//
+//            studyService.deleteStudy(id);
+//
+//            return ResponseUtil.createSuccessResponse(studyResponseDTO, HttpStatus.OK);
+//            //return ResponseEntity.ok(studyResponseDTO);
+//        }
+//        else return ResponseUtil.createErrorResponse(HttpStatus.FORBIDDEN, "Study/PermissionDenied", "the user does not have permission to delete this study");
+//                //ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+//    }
+
+}
---
+++ src/main/java/com/example/comitserver/service/StudyService.java
@@ -0,0 +1,101 @@
+package com.example.comitserver.service;
+
+import com.example.comitserver.dto.CustomUserDetails;
+import com.example.comitserver.dto.StudyRequestDTO;
+import com.example.comitserver.entity.CreatedStudyEntity;
+import com.example.comitserver.entity.StudyEntity;
+import com.example.comitserver.entity.UserEntity;
+import com.example.comitserver.repository.CreatedStudyRepository;
+import com.example.comitserver.repository.StudyRepository;
+import com.example.comitserver.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+@Service
+@Transactional
+public class StudyService {
+
+    private final StudyRepository studyRepository;
+    private final UserRepository userRepository;
+    private final CreatedStudyRepository createdStudyRepository;
+
+    public StudyService(StudyRepository studyRepository, UserRepository userRepository, CreatedStudyRepository createdStudyRepository) {
+        this.studyRepository = studyRepository;
+        this.userRepository = userRepository;
+        this.createdStudyRepository = createdStudyRepository;
+    }
+
+    public List<StudyEntity> showAllStudies() {
+        return studyRepository.findAll();
+    }
+
+    public StudyEntity showStudy(Long id) {
+        return studyRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Study not found with id: " + id));
+    }
+
+    public StudyEntity createStudy(StudyRequestDTO studyRequestDTO, CustomUserDetails customUserDetails) {
+        StudyEntity newStudy = new StudyEntity();
+
+        newStudy.setTitle(studyRequestDTO.getTitle());
+        newStudy.setImageSrc(studyRequestDTO.getImageSrc());
+        // Get the mentor (user) from the repository using the user ID from the customUserDetails
+        UserEntity mentor = userRepository.findById(customUserDetails.getUserId())
+                .orElseThrow(() -> new NoSuchElementException("User not found with id: " + customUserDetails.getUserId()));
+        newStudy.setMentor(mentor);
+        newStudy.setDay(studyRequestDTO.getDay());
+        newStudy.setStartTime(studyRequestDTO.getStartTime());
+        newStudy.setEndTime(studyRequestDTO.getEndTime());
+        newStudy.setLevel(studyRequestDTO.getLevel());
+        newStudy.setTags(studyRequestDTO.getTags());
+        newStudy.setCampus(studyRequestDTO.getCampus());
+        newStudy.setDescription(studyRequestDTO.getDescription());
+        newStudy.setIsRecruiting(studyRequestDTO.getIsRecruiting());
+        newStudy.setSemester(studyRequestDTO.getSemester());
+        studyRepository.save(newStudy);
+
+        CreatedStudyEntity createdStudy = new CreatedStudyEntity();
+        createdStudy.setUser(mentor);
+        createdStudy.setStudy(newStudy);
+        createdStudyRepository.save(createdStudy);
+
+        return newStudy;
+
+    }
+
+    public StudyEntity updateStudy(Long id, StudyRequestDTO studyRequestDTO) {
+        StudyEntity updatingStudy = showStudy(id);
+
+        updatingStudy.setTitle(studyRequestDTO.getTitle());
+        updatingStudy.setImageSrc(studyRequestDTO.getImageSrc());
+        updatingStudy.setDay(studyRequestDTO.getDay());
+        updatingStudy.setStartTime(studyRequestDTO.getStartTime());
+        updatingStudy.setEndTime(studyRequestDTO.getEndTime());
+        updatingStudy.setLevel(studyRequestDTO.getLevel());
+        updatingStudy.setTags(studyRequestDTO.getTags());
+        updatingStudy.setCampus(studyRequestDTO.getCampus());
+        updatingStudy.setDescription(studyRequestDTO.getDescription());
+        updatingStudy.setIsRecruiting(studyRequestDTO.getIsRecruiting());
+        updatingStudy.setSemester(studyRequestDTO.getSemester());
+        studyRepository.save(updatingStudy);
+
+        return updatingStudy;
+    }
+
+    public void deleteStudy(Long id) {
+        createdStudyRepository.deleteByStudyId(id);
+        StudyEntity deletingStudy = showStudy(id);
+        studyRepository.delete(deletingStudy);
+    }
+
+    public Boolean identification(Long id, CustomUserDetails customUserDetails) {
+        StudyEntity study = showStudy(id);
+        Long mentorId = study.getMentor().getId();
+        Long requesterId = customUserDetails.getUserId();
+        return Objects.equals(requesterId, mentorId);
+    }
+}
