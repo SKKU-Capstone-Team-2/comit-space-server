50fc2f0c72472d30b1f88fce3b9d830405ebcaf2
박찬서(Park Chanseo) <101254306+Neoul03@users.noreply.github.com>
2025-05-26 18:54:48 +0900

    feat: Reservation API

---
+++ src/main/java/com/example/comitserver/controller/ReservationAdminController.java
@@ -0,0 +1,40 @@
+package com.example.comitserver.controller;
+
+import com.example.comitserver.dto.ServerResponseDTO;
+import com.example.comitserver.dto.ReservationResponseDTO;
+import com.example.comitserver.service.ReservationService;
+import com.example.comitserver.utils.ResponseUtil;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/admin")
+public class ReservationAdminController {
+    @Autowired
+    private ReservationService reservationService;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @PatchMapping("/reservations/{id}/accept")
+    public ResponseEntity<ServerResponseDTO> acceptReservation(@PathVariable Long id) {
+        try {
+            ReservationResponseDTO dto = reservationService.acceptReservation(id);
+            return ResponseUtil.createSuccessResponse(dto, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseUtil.createErrorResponse(HttpStatus.BAD_REQUEST, "reservation/error", e.getMessage());
+        }
+    }
+
+    @PatchMapping("/reservations/{id}/reject")
+    public ResponseEntity<ServerResponseDTO> rejectReservation(@PathVariable Long id) {
+        try {
+            ReservationResponseDTO dto = reservationService.rejectReservation(id);
+            return ResponseUtil.createSuccessResponse(dto, HttpStatus.OK);
+        } catch (Exception e) {
+            return ResponseUtil.createErrorResponse(HttpStatus.BAD_REQUEST, "reservation/error", e.getMessage());
+        }
+    }
+} 
