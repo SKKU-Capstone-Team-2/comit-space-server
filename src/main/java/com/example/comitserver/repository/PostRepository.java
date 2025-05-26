fd5c61386758fbc8ee381cce081b07e602a886c1
jaesoonkim <winnercup1602@gmail.com>
2025-05-26 17:24:03 +0900

    feat: add getPostByGroup method

--- src/main/java/com/example/comitserver/repository/PostRepository.java
+++ src/main/java/com/example/comitserver/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package com.example.comitserver.repository;
 
 import com.example.comitserver.entity.PostEntity;
+import com.example.comitserver.entity.enumeration.GroupType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
+    List<PostEntity> findByGroupIdAndGroupType(Long groupId, GroupType groupType);
 }
