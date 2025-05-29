01501cbe3be5ab372f9d0371af9145368100dfa2
yh-skku <yh03061@g.skku.edu>
2025-05-29 13:49:47 +0900

    feat: implement event join request to mapping Wait/Accept/Reject and staff can change this state

--- src/main/java/com/example/comitserver/controller/EventController.java
+++ src/main/java/com/example/comitserver/controller/EventController.java
@@ -3,6 +3,8 @@ package com.example.comitserver.controller;
 import com.example.comitserver.dto.*;
 import com.example.comitserver.entity.*;
 import com.example.comitserver.entity.enumeration.JoinState;
+import com.example.comitserver.repository.CreatedEventRepository;
+import com.example.comitserver.repository.CreatedStudyRepository;
 import com.example.comitserver.repository.EventRepository;
 import com.example.comitserver.service.EventService;
 import com.example.comitserver.service.UserService;
@@ -16,7 +18,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,13 +32,15 @@ public class EventController {
     private final ModelMapper modelMapper;
     private final EventRepository eventRepository;
     private final UserService userService;
+    private final CreatedEventRepository createdEventRepository;
 
     @Autowired
-    public EventController(EventService eventService, ModelMapper modelMapper, EventRepository eventRepository, UserService userService) {
+    public EventController(EventService eventService, ModelMapper modelMapper, EventRepository eventRepository, UserService userService, CreatedEventRepository createdEventRepository) {
         this.eventService = eventService;
         this.modelMapper = modelMapper;
         this.eventRepository = eventRepository;
         this.userService = userService;
+        this.createdEventRepository = createdEventRepository;
     }
 
     @GetMapping("/events")
@@ -69,7 +76,39 @@ public class EventController {
         List<UserResponseDTO> userDTOs = createdEventEntities.stream().map(CreatedEventEntity::getUser).map(userEntity -> modelMapper.map(userEntity, UserResponseDTO.class)).toList();
 
         return ResponseUtil.createSuccessResponse(userDTOs, HttpStatus.OK);
-        
+
+    }
+
+    @PatchMapping("/events/{id}/{memberId}")
+    public ResponseEntity<ServerResponseDTO> patchState(@PathVariable Long id, @PathVariable Long memberId, @RequestBody Map<String, String> body, @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        UserEntity user = userService.getUserProfile(customUserDetails.getUserId());
+
+        if (!user.getIsStaff()) return ResponseUtil.createErrorResponse(HttpStatus.FORBIDDEN, "Event/NotStaff", "the user is not a staff member");
+
+        // 해당 이벤트에 상태 수정하려는 멤버 아이디가 존재하지 않는다면 에러
+        if(!createdEventRepository.existsByEventIdAndUserId(id, memberId)) return ResponseUtil.createErrorResponse(HttpStatus.NOT_FOUND, "Event/CannotFindId", "event join request with that member id not found");
+
+        Optional<CreatedEventEntity> createdEvent = createdEventRepository.findByEventIdAndUserId(id,memberId);
+
+        CreatedEventEntity entity = createdEvent.get();
+
+        // 이벤트 가입 요청 상태가 Wait가 아니라면 에러
+        if (!entity.getState().equals(JoinState.Wait)) return ResponseUtil.createErrorResponse(HttpStatus.BAD_REQUEST, "EventJoin/JoinState", "event join request must be Wait");
+
+        String joinStateStr = body.get("joinState");
+        JoinState newState;
+        try {
+            newState = JoinState.valueOf(joinStateStr); // 문자열 → enum
+        } catch (IllegalArgumentException e) {
+            return ResponseUtil.createErrorResponse(HttpStatus.BAD_REQUEST, "InvalidJoinState", "joinState must be Accept or Reject");
+        }
+
+        entity.setState(newState);
+        createdEventRepository.save(entity);
+
+        Map<String, String> joinState = new HashMap<>();
+        joinState.put("joinState", entity.getState().toString());
+        return ResponseUtil.createSuccessResponse(joinState, HttpStatus.OK);
 
     }
 
