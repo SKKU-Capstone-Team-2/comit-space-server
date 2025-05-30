6f765311950909162b0a61843fda18ee549fb0ae
yh-skku <yh03061@g.skku.edu>
2025-05-30 19:05:20 +0900

    add: implement event startDate/endDate and add date validation and add duplicate study/event check function

--- src/main/java/com/example/comitserver/repository/StudyRepository.java
+++ src/main/java/com/example/comitserver/repository/StudyRepository.java
@@ -1,7 +1,20 @@
 package com.example.comitserver.repository;
 
 import com.example.comitserver.entity.StudyEntity;
+import com.example.comitserver.entity.UserEntity;
+import com.example.comitserver.entity.enumeration.Day;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 // spring data JPA에서 자동으로 CRUD 메서드 만들어줌
-public interface StudyRepository extends JpaRepository<StudyEntity, Long> {}
+public interface StudyRepository extends JpaRepository<StudyEntity, Long> {
+    boolean existsByMentorAndDayAndStartTimeAndEndTime(
+            UserEntity mentor,
+            Day day,
+            String startTime,
+            String endTime
+    );
+
+}
